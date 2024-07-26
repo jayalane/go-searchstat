7f2f940254a602f3ae11616c0d7ad10338947ccd
Chris Lane <chris@lane-jayasinha.com>
2024-07-26 07:03:07 -0700

    could have been a shell script

---
+++ cmd/rmFile/README.md
@@ -0,0 +1,19 @@
+Rm File
+===========
+
+This builds a binary takes a file and unlinks
+all the files in it.  
+
+So create a config.txt like this:
+
+```
+fileToRm = ./files.txt
+doubleCheck = danger
+```
+
+and run the binary.
+
+
+
+
+
---
+++ cmd/rmFile/main.go
@@ -0,0 +1,110 @@
+// -*- tab-width: 2 -*-
+
+package main
+
+import (
+	"bufio"
+	"errors"
+	"io"
+	_ "net/http/pprof" //nolint:gosec
+	"os"
+
+	count "github.com/jayalane/go-counter"
+	globals "github.com/jayalane/go-globals"
+)
+
+var g globals.Global
+
+var defaultConfig = `#
+fileToRm = .
+debugLevel = network
+profListen = localhost:8002
+doubleCheck = no
+# comments
+`
+
+func lineHandler(fn string) {
+	g.Ml.La("Checking for deletion {", fn, "}")
+
+	fi, err := os.Lstat(fn)
+	if err != nil {
+		count.IncrSyncSuffix("file-handler-stat-err", "handler")
+		g.Ml.La("Error on stat", fn, err)
+
+		return
+	}
+
+	if !fi.Mode().IsRegular() {
+		count.IncrSyncSuffix("fileb-handler-skip-not-regular", "handler")
+		g.Ml.La("Skipping file is not regular", fn)
+
+		return
+	}
+
+	if (*g.Cfg)["doubleCheck"].StrVal == "danger" {
+		err := os.Remove(fn)
+		if err != nil {
+			count.IncrSyncSuffix("fileb-handler-rm-err", "handler")
+			g.Ml.La("Error opening", fn, err)
+
+			return
+		}
+	}
+
+	count.IncrSyncSuffix("fileb-handler-rm-ok", "handler")
+}
+
+func handleFile(theFile string) {
+	g.Ml.La("Reading file deletion list", theFile)
+
+	file, err := os.Open(theFile)
+	if err != nil {
+		if os.IsNotExist(err) {
+			g.Ml.La(theFile, "does not exist, exiting")
+
+			return
+		}
+
+		g.Ml.La("Warning: can't open file deletion list, exiting,", theFile, err.Error())
+
+		return
+	}
+
+	defer file.Close()
+
+	fileReader := bufio.NewReader(file)
+
+	scanner := bufio.NewScanner(fileReader)
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		if err := scanner.Err(); err != nil {
+			if errors.Is(err, io.EOF) {
+				break
+			}
+
+			g.Ml.La("Error reading file deletion list", err)
+
+			return
+		}
+
+		if len(line) > 0 && line[:1] == "#" { // later space then space #
+			continue
+		}
+
+		lineHandler(line)
+	}
+}
+
+func main() {
+	g = globals.NewGlobal(defaultConfig, true)
+
+	count.SetLogInterval(5.0)
+
+	// first start directory
+	theFile := (*g.Cfg)["fileToRm"].StrVal
+
+	handleFile(theFile)
+
+	count.LogCounters()
+}
